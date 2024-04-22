c4798b79ef6f6fdcd69cd7adcd3a50bae5749249
Vincent Lav <vincent00lav@outlook.com>
2024-04-23 00:28:59 +0200

    Update 5

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homePage(): Response
     {
-        return new Response('<strong>Coffee Shop</strong>: A place where you can swim in coffe !');
+        $coffeCount = 45;
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfCoffee' => $coffeCount,
+        ]);
     }
 }
